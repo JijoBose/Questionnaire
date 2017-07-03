ebde0837c7508d63667868dd2a8d3cbcc9a831ce
Jijo Bose <bosejijo@gmail.com>
2017-07-03 15:09:29 +0530

    layout changes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   has_many :options, dependent: :destroy
-  accepts_nested_attributes_for :options, allow_destroy: true
+  accepts_nested_attributes_for :options, allow_destroy: true, :reject_if => proc { |a| a['option_url'].blank? }
 
   def options_for_form
     collection = options.where(question_id: id)
