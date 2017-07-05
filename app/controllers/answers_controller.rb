830d30b85c70f06a6d6ba5c2f434b1deb4343ced
Jijo Bose <bosejijo@gmail.com>
2017-07-05 16:14:35 +0530

    ajax submit

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,7 +2,18 @@ class AnswersController < ApplicationController
 	def create
 		@survey = Survey.find(params[:survey_id])
 		@answer = @survey.answers.create(answer_params)
-		redirect_to survey_path(@survey)
+		#redirect_to survey_path(@survey)
+
+		# respond_to do |format|
+  #     		if @survey.save
+  #       		format.html { redirect_to preview_survey_path, notice: 'Answer was successfully created.' }
+  #       		format.json { render :show, status: :created, location: @answer }
+  #     		else
+  #       		format.html { render :new }
+  #       		format.json { render json: @survey.errors, status: :unprocessable_entity }
+  #     		end
+  #   	end
+
 	end
 
 	private
