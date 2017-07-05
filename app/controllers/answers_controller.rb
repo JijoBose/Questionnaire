7e7ac69e3d25196f878c66e56c805500b40890f5
Jijo Bose <bosejijo@gmail.com>
2017-07-05 16:23:01 +0530

    submit button

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,15 +4,6 @@ class AnswersController < ApplicationController
 		@answer = @survey.answers.create(answer_params)
 		#redirect_to survey_path(@survey)
 
-		# respond_to do |format|
-  #     		if @survey.save
-  #       		format.html { redirect_to preview_survey_path, notice: 'Answer was successfully created.' }
-  #       		format.json { render :show, status: :created, location: @answer }
-  #     		else
-  #       		format.html { render :new }
-  #       		format.json { render json: @survey.errors, status: :unprocessable_entity }
-  #     		end
-  #   	end
 
 	end
 
